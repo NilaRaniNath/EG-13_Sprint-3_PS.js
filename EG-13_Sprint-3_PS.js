cbb0398ebd8506c308b448f0c75d8a4b1332b09d
SportNest <student@sportnest.com>
2026-09-04 20:45:49 +0600

    number of second problem solved

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,16 +1,39 @@
 // 01. Contains Duplicate
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// var containsDuplicate = function(nums) {
+//     const seen = new Set();
+//     for (const num of nums) {
+//         if (seen.has(num)) {
+//             return true;
+//         }
+//         seen.add(num);
+//     }
+//     return false;
+// };
+// console.log("Contains Duplicate:", containsDuplicate([1, 2, 3, 1]));
+
+
+
+
+// 02. Move Zeroes
 /**
  * @param {number[]} nums
- * @return {boolean}
+ * @return {void} 
  */
-var containsDuplicate = function(nums) {
-    const seen = new Set();
-    for (const num of nums) {
-        if (seen.has(num)) {
-            return true;
+var moveZeroes = function(nums) {
+    let lastNonZeroFoundAt = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            let temp = nums[lastNonZeroFoundAt];
+            nums[lastNonZeroFoundAt] = nums[i];
+            nums[i] = temp;
+            lastNonZeroFoundAt++;
         }
-        seen.add(num);
     }
-    return false;
 };
-// console.log("Contains Duplicate:", containsDuplicate([1, 2, 3, 1]));
+// let nums2 = [0, 1, 0, 3, 12];
+// moveZeroes(nums2);
+// console.log("Move Zeroes:", nums2);
