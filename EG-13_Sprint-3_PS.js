69c975e84621b4c2d9b80940144f5f9931cd9341
SportNest <student@sportnest.com>
2026-09-06 01:03:14 +0600

    all problem solved

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,39 +1,39 @@
 // 01. Contains Duplicate
-// /**
-//  * @param {number[]} nums
-//  * @return {boolean}
-//  */
-// var containsDuplicate = function(nums) {
-//     const seen = new Set();
-//     for (const num of nums) {
-//         if (seen.has(num)) {
-//             return true;
-//         }
-//         seen.add(num);
-//     }
-//     return false;
-// };
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const seen = new Set();
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return true;
+        }
+        seen.add(num);
+    }
+    return false;
+};
 // console.log("Contains Duplicate:", containsDuplicate([1, 2, 3, 1]));
 
 
 
 
 // 02. Move Zeroes
-// /**
-//  * @param {number[]} nums
-//  * @return {void} 
-//  */
-// var moveZeroes = function(nums) {
-//     let lastNonZeroFoundAt = 0;
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums[i] !== 0) {
-//             let temp = nums[lastNonZeroFoundAt];
-//             nums[lastNonZeroFoundAt] = nums[i];
-//             nums[i] = temp;
-//             lastNonZeroFoundAt++;
-//         }
-//     }
-// };
+/**
+ * @param {number[]} nums
+ * @return {void} 
+ */
+var moveZeroes = function(nums) {
+    let lastNonZeroFoundAt = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            let temp = nums[lastNonZeroFoundAt];
+            nums[lastNonZeroFoundAt] = nums[i];
+            nums[i] = temp;
+            lastNonZeroFoundAt++;
+        }
+    }
+};
 // let nums2 = [0, 1, 0, 3, 12];
 // moveZeroes(nums2);
 // console.log("Move Zeroes:", nums2);
@@ -43,26 +43,26 @@
 
 // 03. Valid Anagram
 
-// /**
-//  * @param {string} s
-//  * @param {string} t
-//  * @return {boolean}
-//  */
-// var isAnagram = function(s, t) {
-//     if (s.length !== t.length) return false;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
     
-//     const count = {};
-//     for (let char of s) {
-//         count[char] = (count[char] || 0) + 1;
-//     }
+    const count = {};
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
     
-//     for (let char of t) {
-//         if (!count[char]) return false;
-//         count[char]--;
-//     }
+    for (let char of t) {
+        if (!count[char]) return false;
+        count[char]--;
+    }
     
-//     return true;
-// };
+    return true;
+};
 // console.log("Valid Anagram:", isAnagram("anagram", "nagaram"));
 
 
@@ -71,26 +71,26 @@
 
 // 04. Ransom Note
 
-// /**
-//  * @param {string} ransomNote
-//  * @param {string} magazine
-//  * @return {boolean}
-//  */
-// var canConstruct = function(ransomNote, magazine) {
-//     const count = {};
-//     for (let char of magazine) {
-//         count[char] = (count[char] || 0) + 1;
-//     }
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    const count = {};
+    for (let char of magazine) {
+        count[char] = (count[char] || 0) + 1;
+    }
     
-//     for (let char of ransomNote) {
-//         if (!count[char] || count[char] === 0) {
-//             return false;
-//         }
-//         count[char]--;
-//     }
+    for (let char of ransomNote) {
+        if (!count[char] || count[char] === 0) {
+            return false;
+        }
+        count[char]--;
+    }
     
-//     return true;
-// };
+    return true;
+};
 // console.log(" Ransom Note:", canConstruct("aa", "aab"));
 
 
@@ -98,23 +98,23 @@
 
 // 05. Majority Element
 
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var majorityElement = function(nums) {
-//     let candidate = null;
-//     let count = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate = null;
+    let count = 0;
     
-//     for (let num of nums) {
-//         if (count === 0) {
-//             candidate = num;
-//         }
-//         count += (num === candidate) ? 1 : -1;
-//     }
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
     
-//     return candidate;
-// };
+    return candidate;
+};
 
 // console.log("Majority Element:", majorityElement([2, 2, 1, 1, 1, 2, 2]));
 
@@ -156,7 +156,7 @@ var threeSum = function(nums) {
     
     return result;
 };
-console.log("3Sum:", threeSum([-1, 0, 1, 2, -1, -4]));
+// console.log("3Sum:", threeSum([-1, 0, 1, 2, -1, -4]));
 
 
 
@@ -164,27 +164,27 @@ console.log("3Sum:", threeSum([-1, 0, 1, 2, -1, -4]));
 
 // 07. Subarray Sum Equals K
 
-// /**
-//  * @param {number[]} nums
-//  * @param {number} k
-//  * @return {number}
-//  */
-// var subarraySum = function(nums, k) {
-//     let count = 0;
-//     let sum = 0;
-//     const map = new Map();
-//     map.set(0, 1);
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let count = 0;
+    let sum = 0;
+    const map = new Map();
+    map.set(0, 1);
     
-//     for (let num of nums) {
-//         sum += num;
-//         if (map.has(sum - k)) {
-//             count += map.get(sum - k);
-//         }
-//         map.set(sum, (map.get(sum) || 0) + 1);
-//     }
+    for (let num of nums) {
+        sum += num;
+        if (map.has(sum - k)) {
+            count += map.get(sum - k);
+        }
+        map.set(sum, (map.get(sum) || 0) + 1);
+    }
     
-//     return count;
-// };
+    return count;
+};
 // console.log("Subarray Sum Equals K:", subarraySum([1, 1, 1], 2));
 
 
@@ -195,21 +195,21 @@ console.log("3Sum:", threeSum([-1, 0, 1, 2, -1, -4]));
 
 // 08. Top K Frequent Elements
 
-// /**
-//  * @param {number[]} nums
-//  * @param {number} k
-//  * @return {number[]}
-//  */
-// var topKFrequent = function(nums, k) {
-//     const map = new Map();
-//     for (let num of nums) {
-//         map.set(num, (map.get(num) || 0) + 1);
-//     }
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const map = new Map();
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
     
-//     return Array.from(map.keys())
-//         .sort((a, b) => map.get(b) - map.get(a))
-//         .slice(0, k);
-// };
+    return Array.from(map.keys())
+        .sort((a, b) => map.get(b) - map.get(a))
+        .slice(0, k);
+};
 
 // console.log("Top K Frequent Elements:", topKFrequent([1, 1, 1, 2, 2, 3], 2));
 
@@ -218,32 +218,32 @@ console.log("3Sum:", threeSum([-1, 0, 1, 2, -1, -4]));
 
 
 // 09. Longest Consecutive Sequence
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var longestConsecutive = function(nums) {
-//     if (nums.length === 0) return 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    if (nums.length === 0) return 0;
     
-//     const set = new Set(nums);
-//     let longest = 0;
+    const set = new Set(nums);
+    let longest = 0;
     
-//     for (let num of set) {
-//         if (!set.has(num - 1)) {
-//             let currentNum = num;
-//             let currentStreak = 1;
+    for (let num of set) {
+        if (!set.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
             
-//             while (set.has(currentNum + 1)) {
-//                 currentNum++;
-//                 currentStreak++;
-//             }
+            while (set.has(currentNum + 1)) {
+                currentNum++;
+                currentStreak++;
+            }
             
-//             longest = Math.max(longest, currentStreak);
-//         }
-//     }
+            longest = Math.max(longest, currentStreak);
+        }
+    }
     
-//     return longest;
-// };
+    return longest;
+};
 // console.log("Longest Consecutive Sequence:", longestConsecutive([100, 4, 200, 1, 3, 2]));
 
 
@@ -252,28 +252,28 @@ console.log("3Sum:", threeSum([-1, 0, 1, 2, -1, -4]));
 
 // 10. Sort Colors
 
-// /**
-//  * @param {number[]} nums
-//  * @return {void} Do not return anything, modify nums in-place instead.
-//  */
-// var sortColors = function(nums) {
-//     let low = 0;
-//     let mid = 0;
-//     let high = nums.length - 1;
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let low = 0;
+    let mid = 0;
+    let high = nums.length - 1;
     
-//     while (mid <= high) {
-//         if (nums[mid] === 0) {
-//             [nums[low], nums[mid]] = [nums[mid], nums[low]];
-//             low++;
-//             mid++;
-//         } else if (nums[mid] === 1) {
-//             mid++;
-//         } else {
-//             [nums[mid], nums[high]] = [nums[high], nums[mid]];
-//             high--;
-//         }
-//     }
-// };
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
+};
 
 // let nums10 = [2, 0, 2, 1, 1, 0];
 // sortColors(nums10);
